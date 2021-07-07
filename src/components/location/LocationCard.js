6f3958ee809ae8bd56d9724cc0629f5b86599b16
Thomas Witherow <twitherow@bellsouth.net>
2021-07-07 16:28:25 -0500

    ehh it works

---
+++ src/components/location/LocationCard.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./Location.css"
+
+export const LocationCard = ({ location }) => (
+    <section className="location">
+        {/* <address className="location__address">{location.address}</address> */}
+        {/* <div className="location__squareFootage">{location.squareFootage}</div>
+        <div className="location__handicapAccessible">{location.handicapAccessible}</div> */}
+    </section>
+)
